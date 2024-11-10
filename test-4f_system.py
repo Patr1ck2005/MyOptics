4b2abdb91e21806c3333138eca50fa3ef781b484
A419 GPU <3531176373@qq.com>
2024-11-10 13:16:44 +0800

    A419 使用

--- test-4f_system.py
+++ test-4f_system.py
@@ -5,14 +5,16 @@ from visualization.plotter import Plotter
 
 # Define parameters
 wavelength = 0.550  # Wavelength in micrometers
-f1 = 40*1e3
+f1 = 4*1e3
 f2 = 300*1e3
-w_f = 2*1e3  # Beam waist
+w_f = 0.5*1e3  # Beam waist
+initial_angle = wavelength/PI/w_f
 focal_angle = np.arctan(w_f/f1)
 w_0 = wavelength/PI/focal_angle
 
-sim_size = w_f*4  # Simulation size
-mesh = 1024*6+1  # Mesh size ( +1 to maintain central symmetry)
+w_m = w_f
+sim_size = w_m*5  # Simulation size
+mesh = 1024*10+1  # Mesh size ( +1 to maintain central symmetry)
 x = np.linspace(-sim_size, sim_size, mesh)
 y = np.linspace(-sim_size, sim_size, mesh)
 
@@ -25,8 +27,8 @@ initial_field = np.exp(-(x[:, None] ** 2 + y[None, :] ** 2) / w_0 ** 2)
 optical_system = OpticalSystem(wavelength, x, y, initial_field)
 # optical_system.add_element(PhasePlate(z_position=1, phase_function=lambda X, Y: np.exp(1j * np.arctan2(Y, X))))
 
-# optical_system.add_element(Grating(z_position=0, period=1, amplitude=1))
-optical_system.add_element(BlazedGrating(z_position=0, period=1000/600, blaze_angle=np.deg2rad(17.46/2)))
+# |--f1--|ObjectLens|--f1--|----f2----|Lens1|----f2----|----f2--|Lens2|----f2--|-----d2---------------------------------
+# optical_system.add_element(BlazedGrating(z_position=0, period=1000/600, blaze_angle=np.deg2rad(17.46)))
 optical_system.add_element(ObjectLens(z_position=f1, focal_length=f1))
 optical_system.add_element(Lens(z_position=f2+2*f1, focal_length=f2))
 optical_system.add_element(Lens(z_position=f2*2+2*f1+f2, focal_length=f2))
@@ -56,7 +58,7 @@ if plot_longitudinal_section:  # independently of cross_sections
     coord_axis, z_coords, intensity, phase = (
         optical_system.propagate_to_longitudinal_section(direction='x',
                                                          position=0.0,
-                                                         num_z=128*10,
+                                                         num_z=128*1,
                                                          z_max=f2*2+2*f1+f2*2,
                                                          propagation_mode='Rigorous'))  # Fresnel | Rigorous
 
@@ -64,4 +66,4 @@ if plot_longitudinal_section:  # independently of cross_sections
     plotter.plot_longitudinal_section(coord_axis, z_coords, intensity, phase,
                                       save_label='test',
                                       show=True,
-                                      norm_vmin=(1/np.e)/10)
+                                      norm_vmin=(1/np.e)/(w_m/w_0)**2)
